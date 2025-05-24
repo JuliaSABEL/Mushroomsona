98d71b5af19fd1a93ce1cff90e5b7081110bc16b
JuliaSABEL <sabelnikova.j.partner@gmail.com>
2025-05-24 20:32:07 +0300

    Added Mushroom Customization Reset

---
+++ Assets/Mushroomsona/Scripts/DataManagement/MushroomDataManager.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MushroomDataManager : MonoBehaviour
+{
+    [SerializeField] private List<string> _defaultSceneElements;
+    
+    public static MushroomDataManager Instance { get; private set; }
+    public List<string> DefaultSceneElements { get; private set; }
+    public List<string> ActiveSceneElements { get; set; }
+    
+    
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        DefaultSceneElements = _defaultSceneElements;
+    }
+}
